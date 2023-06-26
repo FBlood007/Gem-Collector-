7a3855873fc0cedaa8f1f941433a55a0aec87b33
FBlood007 <jambhle.omkar300@gmail.com>
2023-06-26 23:55:33 +0530

    add player moment

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
+
+    public  Vector2 movementSpeed;    
+    public Rigidbody2D rb;
+    Vector2 inputX;
+    Vector2 movement;
+   // public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+     
+        Debug.Log(rb.position + " " + inputX);
+
+     
+    }
+    void FixedUpdate()
+    {
+        inputX = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movementSpeed = 4f * inputX;
+        rb.MovePosition(rb.position +  movementSpeed * Time.fixedDeltaTime);
     }
 }
