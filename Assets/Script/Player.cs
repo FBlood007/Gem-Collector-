339eab13c7fc6a3637544f710756a7ae53f4076c
Aashay Kumar <aashaykumar94@gmail.com>
2023-07-08 20:32:28 +0530

    Add block deletion

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,32 +7,55 @@ using UnityEngine.Tilemaps;
 public class Player : MonoBehaviour
 {
 
-    public  Vector2 movementSpeed;    
+    public Vector2 movementSpeed;
     public Rigidbody2D rb;
     Vector2 inputX;
     Vector2 movement;
-   // public GameObject player;
-    // Start is called before the first frame update
+    Direction direction;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    public void DestroyBlock(Tilemap map, Vector3 pos)
+    public void DestroyBlock(Tilemap map)
     {
-        pos.y = Mathf.Floor(pos.y);
-        pos.x = Mathf.Floor(pos.x);
+        Vector3 pos;
+        if (rb != null)
+        {
+            pos.y = Mathf.Floor(rb.position.y);
+            pos.x = Mathf.Floor(rb.position.x);
 
-        map.SetTile(new Vector3Int((int)pos.x, (int)pos.y, 0), null);
+            if (inputX == new Vector2(0, -1)) // down
+            {
+                pos.y = pos.y - 1;
+            }
+            else if (inputX == new Vector2(-1, 0)) // left
+            {
+                pos.x = pos.x - 1;
+            }
+            else if(inputX == new Vector2(0,1)) // up
+            {
+                pos.y = pos.y + 1;
+            }
+            else if(inputX == new Vector2(1, 0)) // right
+            {
+                pos.x = pos.x + 1;
+            }
+            Vector3Int posInt = new Vector3Int((int)pos.x, (int)pos.y, 0);
+            Vector3 cellWorld = map.GetCellCenterWorld(posInt);
+            map.SetTile(posInt, null);
 
-
-        Debug.Log(rb.position + " " + inputX);
+            Debug.Log("posx ,posy" + pos.x + " " + pos.y);
+            Debug.Log(rb.position + " " + inputX);
+        }
     }
     void FixedUpdate()
     {
         inputX = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         movementSpeed = 4f * inputX;
-        rb.MovePosition(rb.position +  movementSpeed * Time.fixedDeltaTime);
+       
+        rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -40,7 +63,17 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag == "TileMap")
         {
             Vector3 pos = collision.gameObject.transform.position;
-            DestroyBlock(collision.gameObject.GetComponent<Tilemap>(), pos);
+            Debug.Log("pos" + pos);
+            DestroyBlock(collision.gameObject.GetComponent<Tilemap>());
         }
     }
+
+    enum Direction
+    {
+        left,
+        right,
+        up,
+        down
+    }
+
 }
